e19e4da8b2c8d77317b7a9bc502134d22f3449cc
anusaxena2701 <30494158+anusaxena2701@users.noreply.github.com>
2020-05-27 02:24:50 +0530

    Update Base.java

--- AppiumGradeup/src/main/java/practise/AppiumGradeup/Base.java
+++ AppiumGradeup/src/main/java/practise/AppiumGradeup/Base.java
@@ -3,6 +3,7 @@ package practise.AppiumGradeup;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.URL;
 import java.util.Properties;
 
@@ -11,9 +12,43 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class Base {
-
+	
+	public static AppiumDriverLocalService service;
+	
+	public AppiumDriverLocalService startServer() 
+	{
+		//start appium server
+		boolean flag=checkIfServerIsRunnning(4723);
+		if(!flag)
+		{
+		service=AppiumDriverLocalService.buildDefaultService();
+		service.start();
+		}
+		return service;
+	 }
+	
+	
+public static boolean checkIfServerIsRunnning(int port) {
+		
+		boolean isServerRunning = false;
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(port);
+			
+			serverSocket.close();
+		} catch (IOException e) {
+			//If control comes here, then it means that the port is in use
+			isServerRunning = true;
+		} finally {
+			serverSocket = null;
+		}
+		return isServerRunning;
+	}
+	
+	
 	public static  AndroidDriver<AndroidElement> capabilities(String appName) throws IOException {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
@@ -26,10 +61,10 @@ public class Base {
 
 				File appDir= new File("src");
 				File app= new File(appDir,(String) prop.get(appName));
-				
+				String device=(String)prop.get("device");
 				DesiredCapabilities cap = new DesiredCapabilities();
 				cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "qwa6emqwifa6n7wc");
+		        cap.setCapability(MobileCapabilityType.DEVICE_NAME, device);
 		        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
 		        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 		        cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 14);
